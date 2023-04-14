dffba09baca3917b82743beb661255f4f83edac0
Mario <Mario@DESKTOP-GBO7PE3>
2023-04-14 12:53:25 +0200

    Animations added

--- CarTuningConfigurator/MainWindow.xaml.cs
+++ CarTuningConfigurator/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -27,16 +28,51 @@ namespace CarTuningConfigurator
         public MainWindow()
         {
             InitializeComponent();
-            updateImage();
-            
-        }
-
-        private void updateImage()
-        {
             Uri uri = new Uri(model.Cars[index].Image, UriKind.Relative);
             BitmapImage imageBItmap = new BitmapImage(uri);
+
             selectedCarImage.Source = imageBItmap;
-                
+
+            // Update the label and triangle visibility
+            if (index == 0)
+            {
+                triangleLeft.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                triangleLeft.Visibility = Visibility.Visible;
+            }
+
+            if (index == model.Cars.Count - 1)
+            {
+                triangleRight.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                triangleRight.Visibility = Visibility.Visible;
+            }
+
+            lblBrandModel.Content = model.Cars[index].Brand + " " + model.Cars[index].Model;
+            lblPrice.Content = $"{model.Cars[index].Price}$";
+        }
+
+        private async void updateImage()
+        {
+            
+            var animation1 = (Storyboard)FindResource("ImageTransitionout");
+            animation1.Begin(selectedCarImage);
+            await Task.Delay(500);
+            
+            Uri uri = new Uri(model.Cars[index].Image, UriKind.Relative);
+            BitmapImage imageBItmap = new BitmapImage(uri);
+
+            
+            var animation2 = (Storyboard)FindResource("ImageTransitionin");
+            
+            selectedCarImage.Source = imageBItmap;
+            animation2.Begin(selectedCarImage);
+
+            // Update the label and triangle visibility
             if (index == 0)
             {
                 triangleLeft.Visibility = Visibility.Hidden;
